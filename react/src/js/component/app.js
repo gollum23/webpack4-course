e1640303e010a2ce695e2a4d9f802c5aec022e21
Diego Forero <gollum23@gmail.com>
2019-09-26 17:37:52 -0500

    :wrench: Playing with React

--- react/src/js/component/app.js
+++ react/src/js/component/app.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './data'
+import Loader from './loader'
+console.log(data)
 
 function App() {
+  const [loaderList, setLoaderList] = useState([])
+
+  function handleClick() {
+    setLoaderList(data.loaders)
+  }
   return (
     <div>
-      que linda aplicación hecha en React.js :)
+      Que linda aplicación hecha en React.js :)
+      <ul>
+        {
+          loaderList.map(item => <Loader {...item} key={item.id} />)
+        }
+      </ul>
+      <button onClick={handleClick}>Mostrar lo aprendido hasta el momento</button>
     </div>
   )
 }
