5ddf764732617379026c9660724894c380ea76c9
Diego Forero <gollum23@gmail.com>
2019-09-26 14:43:47 -0500

    :wrench: Loaders

---
+++ cssStyleLoader/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, 'src/js/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+}
